90dbb1fbfba5f14ef4b2a22116d5b2a5c8754086
Pierce <ppuckette@gmail.com>
2022-06-11 00:07:49 -0400

    useEffect cleanup function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+	// setup state variable to retrieve the size of browser window
+	const [size, setSize] = useState(window.innerWidth);
+
+	const checkSize = () => {
+		setSize(window.innerWidth);
+	};
+
+	useEffect(() => {
+		console.log("useEffect");
+		window.addEventListener("resize", checkSize);
+		// cleanup function
+		return () => {
+			console.log("cleanup");
+			// remove event listners that were previous created
+			window.removeEventListener("resize", checkSize);
+		};
+	});
+
+	console.log("render");
+
+	return (
+		<>
+			<h1>window</h1>
+			<h2>{size} PX</h2>
+		</>
+	);
 };
 
 export default UseEffectCleanup;
