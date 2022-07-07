46022f8c6f53c2318143882f01f2f6ea26432573
Pierce <ppuckette@gmail.com>
2022-07-07 19:05:27 -0400

    enter multiple input to create list to display

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,44 +8,68 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
 	const [firstName, setFirstName] = useState("");
 	const [email, setEmail] = useState("");
-  // handler for form submited via event
-  const handleSubmit = (e) => {
-    // prevents the page from being refreshed
+	const [people, setPeople] = useState([]);
+
+	// handler for form submited via event
+	const handleSubmit = (e) => {
+		// prevents the page from being refreshed
 		e.preventDefault();
-		console.log(firstName, email);
+		if (firstName && email) {
+			// lazy way to setup a unique id for each item in the array
+			const person = { id: new Date().getTime().toString(), firstName, email };
+			setPeople((people) => {
+				// amend the people array with a new person object
+				return [...people, person];
+			});
+			// clear the state values of firstName and email
+			setFirstName("");
+			setEmail("");
+		} else {
+			console.log("empty values");
+		}
 	};
 
 	return (
 		<>
-      <article>
-        {/* submit the data on the form when the form changes or you click a button */}
+			<article>
+				{/* submit the data on the form when the form changes or you click a button */}
 				<form className="form" onSubmit={handleSubmit}>
-          <div className="form-control">
-            {/* the label for the input field collecting namd  */}
+					<div className="form-control">
+						{/* the label for the input field collecting namd  */}
 						<label htmlFor="firstName">Name : </label>
 						<input
 							type="text"
 							id="firstName"
 							name="firstName"
-              value={firstName}
-              // collects the event data from the input and updates the 
+							value={firstName}
+							// collects the event data from the input and updates the
 							onChange={(e) => setFirstName(e.target.value)}
 						/>
 					</div>
-          <div className="form-control">
-            {/* the label and input for the email address data */}
+					<div className="form-control">
+						{/* the label and input for the email address data */}
 						<label htmlFor="email">Email : </label>
 						<input
 							type="text"
 							id="email"
 							name="email"
-              value={email}
-              // update state value with the event.target value
+							value={email}
+							// update state value with the event.target value
 							onChange={(e) => setEmail(e.target.value)}
 						/>
 					</div>
 					<button type="submit">add person</button>
-				</form>
+        </form>
+        {/* iterate the new item added to the array and display them on screen */}
+				{people.map((person) => {
+					const { id, firstName, email } = person;
+					return (
+						<div className="item" key={id} >
+							<h4>{firstName}</h4>
+							<p>{email}</p>
+						</div>
+					);
+				})}
 			</article>
 		</>
 	);
