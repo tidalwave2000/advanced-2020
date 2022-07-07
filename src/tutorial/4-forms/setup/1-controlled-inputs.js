356a4adcd78a83f73d666c79615e0ee9b10ff13c
Pierce <ppuckette@gmail.com>
2022-07-07 18:20:39 -0400

    capture event data and update state with it.

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,27 +6,45 @@ import React, { useState } from "react";
 // value, onChange
 
 const ControlledInputs = () => {
-
+	const [firstName, setFirstName] = useState("");
+	const [email, setEmail] = useState("");
+  // handler for form submited via event
   const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("hello world");
-  };
+    // prevents the page from being refreshed
+		e.preventDefault();
+		console.log(firstName, email);
+	};
 
 	return (
 		<>
-			<article>
+      <article>
+        {/* submit the data on the form when the form changes or you click a button */}
 				<form className="form" onSubmit={handleSubmit}>
-					<div className="form-control">
+          <div className="form-control">
+            {/* the label for the input field collecting namd  */}
 						<label htmlFor="firstName">Name : </label>
-						<input type="text" id="firstName" name="firstName" />
+						<input
+							type="text"
+							id="firstName"
+							name="firstName"
+              value={firstName}
+              // collects the event data from the input and updates the 
+							onChange={(e) => setFirstName(e.target.value)}
+						/>
 					</div>
-					<div className="form-control">
+          <div className="form-control">
+            {/* the label and input for the email address data */}
 						<label htmlFor="email">Email : </label>
-						<input type="text" id="email" name="email" />
+						<input
+							type="text"
+							id="email"
+							name="email"
+              value={email}
+              // update state value with the event.target value
+							onChange={(e) => setEmail(e.target.value)}
+						/>
 					</div>
-					<button type="submit">
-						add person
-					</button>
+					<button type="submit">add person</button>
 				</form>
 			</article>
 		</>
